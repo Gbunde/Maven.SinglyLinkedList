b50bcfbae2de905b4afa290ab8630c5e47be8ab7
Gbunde <56592891+Gbunde@users.noreply.github.com>
2019-11-17 17:54:46 -0500

    tests passed

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -3,5 +3,161 @@ package com.zipcodewilmington.singlylinkedlist;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<T extends Comparable<T>> {
+    private Node<T> head;
+    private Node<T> last;
+    private int size;
+
+    public SinglyLinkedList(int size) {
+        this.size = size;
+        this.head = new Node<T>(null, null);
+        Node currentNode = this.head;
+        for (int i = 0; i < size; i++) {
+            currentNode.next = new Node(null, null);
+            currentNode = currentNode.next;
+        }
+        this.last = currentNode;
+
+    }
+
+
+    public SinglyLinkedList() {
+        this.head = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    public class Node<T> {
+        Node<T> next;
+        T data;
+
+        Node(Node next, T data) {
+            this.next = next;
+            this.data = data;
+        }
+    }
+
+    public void add(T t) {
+        Node newNode = new Node<T>(null, t);
+        if (size == 0) {
+            this.head = newNode;
+            this.last = this.head;
+        }
+        else {
+            Node prevLast = this.last;
+            this.last = newNode;
+            prevLast.next = this.last;
+        }
+        this.size++;
+    }
+
+    public void remove(int index) throws IndexOutOfBoundsException{
+        checkValidIndex(index);
+        if (index == 0) {
+            this.head = this.head.next;
+        } else {
+            Node prevNode = this.head;
+            Node currentNode = this.head.next;
+            int i = 1;
+            while (currentNode != null) {
+                if (i == index) {
+                    prevNode.next = currentNode.next;
+                    break;
+                }
+                currentNode = currentNode.next;
+            }
+        }
+        this.size--;
+    }
+
+    public boolean contains(T t) {
+        Node currentNode = this.head;
+        while (currentNode != null) {
+            if (currentNode.data.equals(t)) return true;
+            currentNode = currentNode.next;
+        }
+        return false;
+    }
+
+    public int find(T t) {
+        Node currentNode = this.head;
+        int i = 0;
+        while (currentNode != null) {
+            if (currentNode.data.equals(t)) return i;
+            currentNode = currentNode.next;
+            i++;
+        }
+        return -1;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public T get(int index) throws IndexOutOfBoundsException {
+        checkValidIndex(index);
+        Node currentNode = this.head;
+        int i = 0;
+        while (currentNode != null) {
+            if (i == index) return  (T)currentNode.data;
+            currentNode = currentNode.next;
+            i++;
+        }
+        return null;
+    }
+
+    public SinglyLinkedList<T> reverse() {
+        SinglyLinkedList<T> reversed = new SinglyLinkedList<T>(this.size);
+        Node reverseNode = reversed.head;
+        int i = this.size-1;
+        while (reverseNode != null && i >= 0) {
+            reverseNode.data = this.get(i);
+            reverseNode = reverseNode.next;
+            i--;
+        }
+        return reversed;
+    }
+
+    public SinglyLinkedList<T> copy() {
+        SinglyLinkedList<T> duplicated = new SinglyLinkedList<T>(this.size);
+        Node originalNode = this.head;
+        Node copyNode = duplicated.head;
+        while (originalNode != null) {
+            copyNode.data = originalNode.data;
+            originalNode = originalNode.next;
+            copyNode = copyNode.next;
+        }
+        return duplicated;
+    }
+
+    public void sort() {
+        if (size <= 1) return;
+        Node currentNode = this.head;
+        Node nextNode = this.head.next;
+        while (!isSorted(this)) {
+            T value = (T)currentNode.data;
+            T nextValue = (T)nextNode.data;
+            if (value.compareTo(nextValue) > 0) {
+                currentNode.data = nextValue;
+                nextNode.data = value;
+            }
+            currentNode = nextNode;
+            if (nextNode.equals(last))  nextNode = this.head;
+            else nextNode = nextNode.next;
+        }
+    }
+
+    protected static Boolean isSorted(SinglyLinkedList sll) {
+        for (int i =0; i < sll.size-1; i++) {
+            if (sll.get(i).compareTo(sll.get(i+1)) > 0) return false;
+        }
+        return true;
+    }
+
+    private void checkValidIndex(int index) throws IndexOutOfBoundsException {
+        if (index < 0 | index >= size) throw new IndexOutOfBoundsException();
+    }
+
+
 }
+
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,255 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+    @Test
+    public void add() {
+        SinglyLinkedList<Integer> sll = new SinglyLinkedList<Integer>();
+        Integer i = 1;
+        sll.add(i);
+        Assert.assertEquals(sll.size(), 1);
+        Assert.assertEquals(sll.get(0), i);
+    }
+
+    @Test
+    public void add2() {
+        SinglyLinkedList<Integer> sll = new SinglyLinkedList<Integer>();
+        Integer i = 1;
+        sll.add(i);
+        Integer j = 2;
+        Integer k = 3;
+        Integer x = 4;
+        sll.add(j);
+        sll.add(k);
+        sll.add(x);
+        Assert.assertEquals(sll.size(), 4);
+    }
+
+    @Test
+    public void remove() {
+        SinglyLinkedList sll = new SinglyLinkedList();
+        Integer i = 1;
+        sll.add(i);
+        sll.remove(0);
+        Assert.assertEquals(sll.size(), 0);
+    }
+
+    @Test
+    public void contains() {
+        SinglyLinkedList sll = new SinglyLinkedList();
+        Integer i = 1;
+        sll.add(i);
+        Integer j = 10;
+        sll.add(j);
+        Assert.assertTrue(sll.contains(1));
+        Assert.assertTrue(sll.contains(10));
+    }
+
+    @Test
+    public void contains2() {
+        SinglyLinkedList<String> why = new SinglyLinkedList();
+        String s = "s";
+        String t = "t";
+        String u = "u";
+        why.add(s);
+        why.add(t);
+        why.add(u);
+        Assert.assertTrue(why.contains("u"));
+        Assert.assertTrue(why.contains(u));
+    }
+
+    @Test
+    public void find() {
+        SinglyLinkedList sll2 = new SinglyLinkedList();
+        Integer i = 10;
+        sll2.add(i);
+        Assert.assertTrue(sll2.contains(i));
+        int actual = sll2.find(10);
+        Assert.assertEquals(0, actual);
+    }
+
+    @Test
+    public void find2() {
+        SinglyLinkedList sll2 = new SinglyLinkedList();
+        Integer i = 10;
+        Integer j = 20;
+        Integer k = 30;
+        sll2.add(i);
+        sll2.add(j);
+        sll2.add(k);
+        int actual = sll2.find(5);
+        Assert.assertEquals(-1, actual);
+    }
+
+    @Test
+    public void find3() {
+        SinglyLinkedList<String> why = new SinglyLinkedList();
+        String s = "s";
+        String t = "t";
+        String u = "u";
+        why.add(s);
+        why.add(t);
+        why.add(u);
+        int expected = 2;
+        int actual = why.find(u);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void size() {
+        SinglyLinkedList sll = new SinglyLinkedList();
+        Integer i = 1;
+        sll.add(i);
+        Integer j = 2;
+        sll.add(j);
+        sll.remove(1);
+        Assert.assertEquals(sll.size(), 1);
+    }
+
+    @Test
+    public void get() {
+        SinglyLinkedList sll = new SinglyLinkedList();
+        Integer i = 1;
+        sll.add(i);
+        Assert.assertEquals(i, sll.get(0));
+    }
+
+    @Test
+    public void get2() {
+        SinglyLinkedList sll = new SinglyLinkedList();
+        Integer i = 1;
+        Integer j = 2;
+        Integer k = 3;
+        sll.add(i);
+        sll.add(j);
+        sll.add(k);
+        Assert.assertEquals(k, sll.get(2));
+    }
+
+    @Test
+    public void get3() {
+        SinglyLinkedList sll = new SinglyLinkedList();
+        Integer i = 5;
+        Integer j = 2;
+        Integer k = 6;
+        Integer x = 102;
+        Integer y = 120;
+        Integer z = 0;
+        sll.add(i);
+        sll.add(j);
+        sll.add(k);
+        sll.add(x);
+        sll.add(y);
+        sll.add(z); //before = {5, 2, 6, 102, 120, 0}
+        Assert.assertEquals(z, sll.get(5));
+        sll.sort();
+        Assert.assertEquals(z, sll.get(0));
+    }
+    @Test
+    public void copy() {
+        SinglyLinkedList sll = new SinglyLinkedList();
+        Integer i = 1;
+        Integer j = 2;
+        Integer k = 3;
+        sll.add(i);
+        sll.add(j);
+        sll.add(k);
+        SinglyLinkedList copied = sll.copy();
+        Assert.assertEquals(i, copied.get(0));
+        Assert.assertEquals(j, copied.get(1));
+        Assert.assertEquals(k, copied.get(2));
+        Assert.assertFalse(copied.equals(sll));
+        Assert.assertNotSame(copied, sll);
+    }
+
+    @Test
+    public void sort() {
+        SinglyLinkedList sll = new SinglyLinkedList();
+        Integer i = 1;
+        sll.add(i);
+        sll.sort();
+        Assert.assertEquals(i, sll.get(0));
+    }
+
+    @Test
+    public void sort2() {
+        SinglyLinkedList sll = new SinglyLinkedList();
+        Integer i = 5;
+        Integer j = 2;
+        Integer k = 6;
+        sll.add(i);
+        sll.add(j);
+        sll.add(k);
+        sll.sort();
+        Assert.assertEquals(j, sll.get(0));
+    }
+
+    @Test
+    public void sort3() {
+        SinglyLinkedList sll = new SinglyLinkedList();
+        Integer i = 5;
+        Integer j = 2;
+        Integer k = 6;
+        Integer x = 102;
+        Integer y = 120;
+        Integer z = 0;
+        sll.add(i);
+        sll.add(j);
+        sll.add(k);
+        sll.add(x);
+        sll.add(y);
+        sll.add(z); //before = {5, 2, 6, 102, 120, 0}
+        sll.sort(); //expected = {0, 2, 5, 6, 102, 120
+        Assert.assertTrue(SinglyLinkedList.isSorted(sll));
+    }
+
+    @Test
+    public void reverse() {
+        SinglyLinkedList sll = new SinglyLinkedList();
+        Integer i = 1;
+        Integer j = 2;
+        Integer k = 3;
+        sll.add(i);
+        sll.add(j);
+        sll.add(k);
+        SinglyLinkedList reversed = sll.reverse();
+        Assert.assertEquals(k, reversed.get(0));
+        Assert.assertEquals(j, reversed.get(1));
+        Assert.assertEquals(i, reversed.get(2));
+    }
+
+    @Test
+    public void isSortedTest() {
+        SinglyLinkedList sll = new SinglyLinkedList();
+        Integer i = 1;
+        Integer j = 2;
+        Integer k = 3;
+        sll.add(i);
+        sll.add(j);
+        sll.add(k);
+        Assert.assertTrue(SinglyLinkedList.isSorted(sll));
+    }
+    @Test
+    public void isSortedTest2(){
+        SinglyLinkedList sll = new SinglyLinkedList();
+        Integer i = 5;
+        Integer j = 2;
+        Integer k = 6;
+        Integer x = 102;
+        Integer y = 120;
+        Integer z = 0;
+        sll.add(z);
+        sll.add(j);
+        sll.add(i);
+        sll.add(k);
+        sll.add(x);
+        sll.add(y); //before = {5, 2, 6, 102, 120, 0}
+        Assert.assertTrue(SinglyLinkedList.isSorted(sll));
+    }
 }
+
